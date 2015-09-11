1d7185650e88ee3217b87ea3410a5fddbf60985e
Mark Feltner <mfeltner@widen.com>
2015-09-10 22:24:03 -0500

    test: end-to-end

--- test/all-test.js
+++ test/all-test.js
@@ -3,6 +3,33 @@ var test = require('tape');
 var nowplaying = require('../index');
 
 test('sanity check', function(t){
-    t.plan(1);
+    t.plan(2);
     t.ok(nowplaying);
+
+    var brain = { get: function(foo, cb) { return cb(null, 'plugitin'); } };
+
+    nowplaying.respond({
+        brain: brain,
+        command: 'np',
+        log: {
+            info: console.log,
+            error: console.error
+        },
+        args: '',
+        plugin: {
+            options: {
+                api_key: process.env.LASTFM_API_KEY,
+                secret: process.env.LASTFM_SECRET
+            }
+        },
+        incoming_message: {
+            user_name: 'feltnerm'
+        }
+    }).then(function(result){
+        t.pass(result);
+    }).catch(function(error){
+        t.fail();
+    });
+
+
 });
